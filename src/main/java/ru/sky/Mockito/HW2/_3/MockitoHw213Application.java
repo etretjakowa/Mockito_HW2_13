2276951d8f5f650e360d28ca32186ab5257a24e9
etretjakova <etretjakowa@yandex.ru>
2024-11-10 22:30:12 +0300

    initial commit

---
+++ src/main/java/ru/sky/Mockito/HW2/_3/MockitoHw213Application.java
@@ -0,0 +1,13 @@
+package ru.sky.Mockito.HW2._3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoHw213Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoHw213Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sky/Mockito/HW2/_3/MockitoHw213ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sky.Mockito.HW2._3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoHw213ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
